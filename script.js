09ca057ccdaefd9082574f003aad709cfa88be2b
yyred-max <anay29758@gmail.com>
2026-05-09 19:53:13 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+
+const inputTugas = document.getElementById("inputTugas");
+const btntambah = document.getElementById("btnTambah");
+const daftarTugas = document.getElementById("daftarTugas");
+
+btntambah.addEventListener("click",function() {
+
+    let teksTugas = inputTugas.value;
+
+    if(teksTugas === ""){
+        alert("Data harus dimasukkan!");
+        return;
+    }
+
+    let listbaru = document.createElement("li");
+    let spanbaru = document.createElement("span");
+
+    spanbaru.innerHTML = teksTugas;
+
+    listbaru.appendChild(spanbaru);
+
+    daftarTugas.appendChild(listbaru);
+
+    const warnabaru = document.querySelectorAll("li");
+    warnabaru.forEach((item, index) => {
+        if(index % 2 === 0) {
+            item.style.color = "red";
+        } else {
+            item.style.color = "green";
+        }
+    });
+
+    inputTugas.Value = "";
+
+});
